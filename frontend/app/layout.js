466cb20f3eb5552b79edf7517926a4d9bed8ae6a
joeljarvi <joeljarvi@live.se>
2025-04-15 14:57:38 +0200

    lagt till fonts

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,9 +1,26 @@
 import "./globals.css";
+import localFont from "next/font/local";
+
+const fontAbsans = localFont({
+  src: "./fonts/Absans-Regular.woff",
+  display: "swap",
+  variable: "--font-fontAbsans",
+});
+
+const fontMattoneBlack = localFont({
+  src: "./fonts/Mattone-Black.woff",
+  display: "swap",
+  variable: "--font-fontMattoneBlack",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={`${fontAbsans.variable} ${fontMattoneBlack.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -3,8 +3,14 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="secondary">Button</Button>
-      <Button size="lg">Button</Button>
+      <Button variant="secondary" className="font-absans">
+        Button
+      </Button>
+      <Button size="lg" className="font-mattone-black">
+        Button
+      </Button>
+      <h1 className="font-mattone-black text-6xl">MERA</h1>
+      <h2 className="font-absans text-6xl">Make</h2>
     </div>
   );
 }
