6dd9bb31e4bc0dd9a7a56cfc539342b45771f8ed
Irene Rubio <rubio.hernandez.irene@gmail.com>
2025-04-19 18:08:29 +0200

    buttom colors commit

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,10 +5,31 @@ import ImageRotating from "@/components/ImageRotating";
 export default function Home() {
   return (
     <div>
-      <Button variant="secondary" className="font-absans">
+      <Button variant="secondary" className="font-absans bg-[var(--color-malachite)]">
         Button
       </Button>
-      <Button size="lg" className="font-mattone-black">
+      <Button variant="secondary" className="font-absans bg-[var(--color-amarillo)]">
+        Button
+      </Button>
+      <Button variant="secondary" className="font-absans text-white bg-[var(--color-militar)]">
+        Button
+      </Button>
+      <Button variant="secondary" className="font-absans bg-[var(--color-cenize)]">
+        Button
+      </Button>
+      <Button size="lg" className="font-mattone-black bg-[var(--color-scarlett)]">
+        Button
+      </Button>
+      <Button size="lg" className="font-mattone-black bg-[var(--color-aubergine)]">
+        Button
+      </Button>
+      <Button size="lg" className="font-mattone-black bg-[var(--color-cobalt)]">
+        Button
+      </Button>
+      <Button size="lg" className="font-mattone-black bg-[var(--color-coralred)]">
+        Button
+      </Button>
+      <Button size="lg" className="font-mattone-black bg-[var(--color-ochre)]">
         Button
       </Button>
       <h1 className="font-mattone-black text-6xl flex items-center justify-center ">MERA</h1>
