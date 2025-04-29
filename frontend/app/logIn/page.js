a27ac6d0e7f1cf180653c298e5e85e7263c92f46
Irene Rubio <rubio.hernandez.irene@gmail.com>
2025-04-29 10:24:24 +0200

    commit

--- frontend/app/logIn/page.js
+++ frontend/app/logIn/page.js
@@ -10,7 +10,7 @@ export default function AuthPage() {
     e.preventDefault();
     const payload = { username, password };
     console.log("Logga in:", payload);
-g
+
     setUsername("");
     setPassword("");
   }
