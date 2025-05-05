8eaed681135e144f924ccb0f3ebd95707d7a94c3
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-05-05 17:55:10 +0200

    Added new calendar type

--- frontend/app/schedulePage/page.js
+++ frontend/app/schedulePage/page.js
@@ -1,13 +1,11 @@
 "use client";
 
-import MyCalendar from "@/components/BigCalendar";
+import Calendar from "@/components/Calendar";
 
 export default function SchedulePage() {
   return (
-    <div className="flex flex-col items-center p-20 gap-10 font-instrument">
-      <div className="w-full">
-        <MyCalendar />
-      </div>
+    <div className="flex flex-col items-center px-20 gap-10 font-instrument">
+      <Calendar />
     </div>
   );
 }
