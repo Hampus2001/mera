1103ec752760412eb33e3cc49973c760cb99b74f
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-05-05 13:10:14 +0200

    size on calendar

--- frontend/app/schedulePage/page.js
+++ frontend/app/schedulePage/page.js
@@ -5,7 +5,7 @@ import MyCalendar from "@/components/BigCalendar";
 export default function SchedulePage() {
   return (
     <div className="flex flex-col items-center p-20 gap-10 font-instrument">
-      <div className="w-4/5">
+      <div className="w-full">
         <MyCalendar />
       </div>
     </div>
