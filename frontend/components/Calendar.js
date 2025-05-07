ba94f7b274f4b8e5dec1ebc2552aaaffe0531fa4
Hampus2001 <hampus.soderlund@chasacademy.se>
2025-05-07 14:09:28 +0200

    Added modal that pops up on clicked days coordinates

--- frontend/components/Calendar.js
+++ frontend/components/Calendar.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Calendar() {
   const todaysDate = new Date();
@@ -11,8 +11,27 @@ export default function Calendar() {
   const [year, setYear] = useState(todaysYear);
   const [selectedDate, setSelectedDate] = useState(null);
   const [showModal, setShowModal] = useState(false);
+  const [monthString, setMonthString] = useState("");
+
+  //Get x and y coordinates for modal window
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const getCoordinates = (e) => {
+    const rect = e.target.getBoundingClientRect();
+    setPosition({
+      x: rect.left + window.scrollX,
+      y: rect.top + window.scrollY,
+    });
+  };
+
+  useEffect(() => {
+    console.log(position);
+  }, [position]);
 
   function getDaysInCurrentMonth() {
+    convertMonthToString();
+
     const newCalendar = [];
 
     const currentDate = new Date();
@@ -43,39 +62,42 @@ export default function Calendar() {
     ) {
       newCalendar.push(
         <button
-          onClick={() => {
+          onClick={(e) => {
             if (currentMonth == 0) {
               setSelectedDate(`${currentYear}-${currentMonth}-${i}`);
               setShowModal(true);
+              getCoordinates(e);
             } else {
               setSelectedDate(`${currentYear}-${currentMonth}-${i}`);
               setShowModal(true);
+              getCoordinates(e);
             }
           }}
           key={`prev-${i}`}
-          className="flex hover:cursor-pointer flex-col p-5 gap-2 w-full h-44 rounded-lg bg-red-100 text-red-900 shadow-md"
+          className="flex hover:cursor-pointer flex-col p-5 border-b-2 border-l-2 border-gray-300 w-1/7 aspect-square bg-base-100 text-base-content "
         >
-          <p className="text-2xl font-extrabold">{i}</p>
+          <p className=" font-extrabold">{i}</p>
         </button>
       );
     }
 
     // Fill in days of the current month
     for (let i = 1; i <= daysInMonth; i++) {
-      let currentDayStyle = " bg-red-300 shadow-red-500";
+      let currentDayStyle = " bg-base-200 ";
       if (i == currentDay && month == todaysMonth && year == todaysYear) {
-        currentDayStyle = " bg-red-400 shadow-red-600";
+        currentDayStyle = " bg-base-300 ";
       }
       newCalendar.push(
         <button
-          onClick={() => {
+          onClick={(e) => {
             setSelectedDate(`${currentYear}-${currentMonth + 1}-${i}`);
             setShowModal(true);
+            getCoordinates(e);
           }}
           key={`current-${i}`}
-          className={`flex hover:cursor-pointer flex-col p-5 gap-2 w-full h-44 rounded-lg ${currentDayStyle} text-red-900 shadow-md`}
+          className={`flex hover:cursor-pointer flex-col p-5  border-b-2 border-l-2 border-gray-300 w-1/7 aspect-square  ${currentDayStyle} text-base-content `}
         >
-          <p className="text-2xl font-extrabold">{i}</p>
+          <p className=" font-extrabold">{i}</p>
         </button>
       );
     }
@@ -87,19 +109,21 @@ export default function Calendar() {
     for (let i = 1; i <= remainingDays; i++) {
       newCalendar.push(
         <button
-          onClick={() => {
+          onClick={(e) => {
             if (currentMonth == 11) {
               setSelectedDate(`${currentYear}-${currentMonth + 2}-${i}`);
               setShowModal(true);
+              getCoordinates(e);
             } else {
               setSelectedDate(`${currentYear}-${currentMonth + 2}-${i}`);
               setShowModal(true);
+              getCoordinates(e);
             }
           }}
           key={`next-${i}`}
-          className="flex hover:cursor-pointer flex-col p-5 gap-2 w-full h-44 rounded-lg bg-red-100 text-red-900 shadow-md"
+          className="flex hover:cursor-pointer flex-col p-5 border-b-2 border-l-2 border-gray-300 w-1/7 aspect-square bg-base-100 text-base-content"
         >
-          <p className="text-2xl font-extrabold">{i}</p>
+          <p>{i}</p>
         </button>
       );
     }
@@ -107,85 +131,107 @@ export default function Calendar() {
     setCalendar(newCalendar);
   }
 
+  function convertMonthToString() {
+    if (month === 0) {
+      setMonthString("January");
+    } else if (month === 1) {
+      setMonthString("February");
+    } else if (month === 2) {
+      setMonthString("March");
+    } else if (month === 3) {
+      setMonthString("April");
+    } else if (month === 4) {
+      setMonthString("May");
+    } else if (month === 5) {
+      setMonthString("June");
+    } else if (month === 6) {
+      setMonthString("July");
+    } else if (month === 7) {
+      setMonthString("August");
+    } else if (month === 8) {
+      setMonthString("September");
+    } else if (month === 9) {
+      setMonthString("October");
+    } else if (month === 10) {
+      setMonthString("November");
+    } else if (month === 11) {
+      setMonthString("December");
+    }
+  }
+
   useEffect(() => {
     getDaysInCurrentMonth();
-  }, [month, year]);
+  }, [month]);
 
   return (
     <>
-      <div className="flex flex-col w-full rounded-xl  gap-5 p-10 m-10">
-        <div className="flex gap-10">
-          <select
-            className="flex font-bold w-1/6 outline-none text-red-900 text-2xl p-2 rounded-xl "
-            value={year}
-            onChange={(e) => {
-              setYear(e.target.value);
+      <div className="flex flex-col w-3/4 rounded-xl gap-4">
+        <div className="flex gap-4">
+          <h4 className="w-1/5">
+            {monthString} - {year}
+          </h4>
+          <button
+            className="btn btn-app btn-secondary btn-sm md:btn-md lg:btn-lg"
+            onClick={() => {
+              if (month != 0) {
+                setMonth(month - 1);
+              } else if (month == 0) {
+                setYear(year - 1);
+                setMonth(11);
+              }
             }}
           >
-            <option value={todaysYear - 2}>{todaysYear - 2}</option>
-            <option value={todaysYear - 1}>{todaysYear - 1}</option>
-            <option value={todaysYear}>{todaysYear}</option>
-            <option value={todaysYear + 1}>{todaysYear + 1}</option>
-            <option value={todaysYear + 2}>{todaysYear + 2}</option>
-          </select>
-          <select
-            className="flex font-bold w-1/6 outline-none text-red-900 text-2xl p-2 rounded-xl "
-            value={month}
-            onChange={(e) => {
-              setMonth(e.target.value);
+            Privious
+          </button>
+
+          <button
+            className="btn btn-app btn-secondary btn-sm md:btn-md lg:btn-lg"
+            onClick={() => {
+              if (month != 11) {
+                setMonth(month + 1);
+              } else if (month == 11) {
+                setYear(year + 1);
+                setMonth(0);
+              }
             }}
           >
-            <option value="0">January</option>
-            <option value="1">February</option>
-            <option value="2">Mars</option>
-            <option value="3">April</option>
-            <option value="4">May</option>
-            <option value="5">June</option>
-            <option value="6">Juli</option>
-            <option value="7">August</option>
-            <option value="8">September</option>
-            <option value="9">October</option>
-            <option value="10">November</option>
-            <option value="11">December</option>
-          </select>
+            Next
+          </button>
         </div>
 
         <div className="grid grid-cols-7 grid-rows-1 gap-2">
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             mon
           </p>
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             tue
           </p>
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             wed
           </p>
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             thur
           </p>
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             fri
           </p>
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             sat
           </p>
-          <p className="flex justify-center font-bold text-2xl text-red-900">
+          <p className="flex justify-center font-bold  text-base-content">
             sun
           </p>
         </div>
 
-        <div
-          className={`w-full grid grid-cols-7 ${
-            calendar.length % 10 === 0
-              ? `grid-rows-${calendar.length / 10}`
-              : `grid-rows-${Math.floor(calendar.length / 10) + 1}`
-          } gap-2`}
-        >
+        <div className="flex flex-wrap border-t-2 border-r-2 border-gray-300">
           {calendar}
         </div>
       </div>
       {showModal && (
-        <div className="fixed top-0 bg-white w-full rounded-xl min-h-screen">
+        <div
+          className={`absolute bg-white w-1/6 rounded-xl h-96`}
+          style={{ top: position.y, left: position.x }}
+        >
           <button
             className="flex p-5 text-xl font-bold text-center hover:cursor-pointer"
             onClick={() => setShowModal(false)}
